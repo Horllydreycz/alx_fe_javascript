f5525b9e811ca1365352c8ea42e53e7cb5f880fa
horllydreycz <oluwadaretitus3@gmail.com>
2025-10-26 14:18:51 +0100

    dom

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -134,6 +134,16 @@ function exportQuotes() {
 }
 
 // Import quotes from JSON file
+function importFromJsonFile(event) {
+  const fileReader = new FileReader();
+  fileReader.onload = function (event) {
+    const importedQuotes = JSON.parse(event.target.result);
+    quotes.push(...importedQuotes);
+    saveQuotes();
+    alert("Quotes imported successfully!");
+  };
+  fileReader.readAsText(event.target.files[0]);
+}
 function importQuotes(event) {
   const file = event.target.files[0];
   if (!file) {
